05414bc52b1ccc653290a538de6b29909946dfb3
Julie Turner <julie@julieturner.net>
2022-07-05 17:05:34 +0000

    Update to PnPjs v3

--- samples/ace-mymail/src/adaptiveCardExtensions/myMail/MyMailAdaptiveCardExtension.ts
+++ samples/ace-mymail/src/adaptiveCardExtensions/myMail/MyMailAdaptiveCardExtension.ts
@@ -1,16 +1,15 @@
 import { IPropertyPaneConfiguration } from '@microsoft/sp-property-pane';
 import { BaseAdaptiveCardExtension } from '@microsoft/sp-adaptive-card-extension-base';
 
-import { sp } from "@pnp/sp";
-import { graph } from "@pnp/graph";
-import { Logger, LogLevel, ConsoleListener } from "@pnp/logging";
+// import { sp } from "@pnp/sp";
+// import { graph } from "@pnp/graph";
+// import { Logger, LogLevel, ConsoleListener } from "@pnp/logging";
 
 import { CardView } from './cardView/CardView';
 import { QuickView } from './quickView/QuickView';
 import { MyMailPropertyPane } from './MyMailPropertyPane';
-import { IMyMailService, MyMailService } from './services/mymail.service';
+import { IMyMailService, myMailService, MyMailService } from './services/mymail.service';
 import { Message } from './models/mymail.models';
-
 
 export interface IMyMailAdaptiveCardExtensionProps {
   title: string;
@@ -32,6 +31,8 @@ export default class MyMailAdaptiveCardExtension extends BaseAdaptiveCardExtensi
 > {
   private _deferredPropertyPane: MyMailPropertyPane | undefined;
   private LOG_SOURCE: string = "🔶 MyMailAdaptiveCardExtension";
+  private _waiting;
+  private _calls: number = 0;
 
   public async onInit(): Promise<void> {
 
@@ -39,37 +40,51 @@ export default class MyMailAdaptiveCardExtension extends BaseAdaptiveCardExtensi
       this.cardNavigator.register(CARD_VIEW_REGISTRY_ID, () => new CardView());
       this.quickViewNavigator.register(QUICK_VIEW_REGISTRY_ID, () => new QuickView());
 
-      //Initialize PnPLogger
-      Logger.subscribe(new ConsoleListener());
-      Logger.activeLogLevel = LogLevel.Info;
-
-      //Initialize PnPJs
-      sp.setup({ spfxContext: this.context });
-      graph.setup({ spfxContext: this.context });
-
-      const myMailService = this.context.serviceScope.consume(MyMailService.serviceKey);
-      const myMessages = await myMailService.getMyMail(this.properties.mailType, this.properties.numToReturn);
-
-
       this.state = {
-        messages: myMessages,
+        messages: [],
       };
-      this.refreshMail(myMailService, this.properties.refreshRate);
+
+
+      myMailService.Init(this.context.serviceScope);
+      this._firstLoad();
+
     } catch (err) {
-      Logger.write(`${this.LOG_SOURCE} (onInit) - ${err.message} - `, LogLevel.Error);
+      console.error(`${this.LOG_SOURCE} (onInit) - ${err.message} - `);
     }
 
     return Promise.resolve();
   }
 
-  private async refreshMail(mailService: IMyMailService, delay: number): Promise<any> {
+  private async _firstLoad(): Promise<void> {
+    try {
+      //Re-render when ready or calls > 1000
+      const checkRender = async () => {
+        if (myMailService.ready || this._calls > 600) {
+          stop();
+          this.refreshMail();
+        } else {
+          this._calls++;
+        }
+      };
+
+      const stop = () => {
+        clearInterval(this._waiting);
+      };
+
+      this._waiting = setInterval(checkRender, 100);
+    } catch (err) {
+      console.error(`${this.LOG_SOURCE} (_firstLoad) ${err}`);
+    }
+  }
+
+  private async refreshMail(): Promise<any> {
     while (true) {
-      const myMessages = await mailService.getMyMail(this.properties.mailType, this.properties.numToReturn);
+      const myMessages = await myMailService.getMyMail(this.properties.mailType, this.properties.numToReturn);
       //We don't want to rerender if the count hasn't changed.
       if (myMessages.length != this.state.messages.length) {
         this.setState({ messages: myMessages });
       }
-      await this.delay(delay * 60000);
+      await this.delay(this.properties.refreshRate * 60000);
     }
   }
 
--- samples/ace-mymail/src/adaptiveCardExtensions/myMail/services/mymail.service.ts
+++ samples/ace-mymail/src/adaptiveCardExtensions/myMail/services/mymail.service.ts
@@ -1,14 +1,15 @@
 import { ServiceKey, ServiceScope } from "@microsoft/sp-core-library";
 import { PageContext } from "@microsoft/sp-page-context";
+import { AadTokenProviderFactory } from "@microsoft/sp-http";
 
-import { graph, GraphQueryableCollection } from "@pnp/graph";
+import { graphfi, SPFx, GraphFI } from "@pnp/graph";
 import "@pnp/graph/users";
 import "@pnp/graph/messages";
 
-import { Logger, LogLevel } from "@pnp/logging";
 import { MailType, Message } from "../models/mymail.models";
 
 export interface IMyMailService {
+  readonly ready: boolean;
   getMyMail: (filter: string, maxNumber: number) => Promise<Message[]>;
 }
 
@@ -16,16 +17,24 @@ export class MyMailService implements IMyMailService {
   private LOG_SOURCE: string = "🔶 MyMailService";
   public static readonly serviceKey: ServiceKey<IMyMailService> =
     ServiceKey.create<MyMailService>("MyMailService:IMyMailService", MyMailService);
-  private _serviceScope: ServiceScope;
   private _pageContext: PageContext;
+  private _ready: boolean = false;
+  private _graph: GraphFI;
   private _styleArray: string[] = ['498205', '0b6a0b', '038387', '005b70', '4f6bed', '0078d4', '004e8c', '8764b8', '5c2e91', '881798', 'c239b3', 'e3008c', '750b1c', 'd13438', 'a4262c', 'ca5010', '8e562e', '986f0b', '7a7574', '69797e'];
-  private _selectedStyle: string = this._styleArray[0];
 
-  constructor(serviceScope: ServiceScope) {
+  constructor() { }
+
+  public Init(serviceScope: ServiceScope): void {
     serviceScope.whenFinished(() => {
-      this._serviceScope = serviceScope;
       this._pageContext = serviceScope.consume(PageContext.serviceKey);
+      const aadTokenProviderFactory = serviceScope.consume(AadTokenProviderFactory.serviceKey);
+      this._graph = graphfi().using(SPFx({ pageContext: this._pageContext, aadTokenProviderFactory }));
+      this._ready = true;
     });
+  }
+
+  public get ready(): boolean {
+    return this._ready;
   }
 
   public async getMyMail(filter: string, maxNumber: number): Promise<Message[]> {
@@ -39,14 +48,12 @@ export class MyMailService implements IMyMailService {
     }
 
     try {
-      const messagesQuery = GraphQueryableCollection(graph.me, "mailFolders/Inbox/messages");
       const today: Date = new Date();
-      const messages = await messagesQuery
-        .select("id,receivedDateTime,subject,importance,isRead,webLink,inferenceClassification,from")
+      const messages: Message[] = await this._graph.me.messages
+        .select("id", "receivedDateTime", "subject", "importance", "isRead", "webLink", "inferenceClassification", "from")
         .filter(filterString)
         .orderBy('receivedDateTime', false)
-        .top(maxNumber)
-        .get<{ id: string, receivedDateTime: string, subject: string, importance: string, isRead: boolean, webLink: string, inferenceClassification: string, isToday: boolean, from: { emailAddress: { name: string, address: string, initials: string, bgColor: string } } }[]>();
+        .top(maxNumber)();
       let currentUserDomain = this._pageContext.user.email.split("@")[1];
       messages.map((m) => {
         const recievedDate: Date = new Date(m.receivedDateTime);
@@ -74,8 +81,10 @@ export class MyMailService implements IMyMailService {
       });
       retVal = messages;
     } catch (err) {
-      Logger.write(`${this.LOG_SOURCE} (getMyMail) - ${err.message} - `, LogLevel.Error);
+      console.error(`${this.LOG_SOURCE} (getMyMail) - ${err.message} - `);
     }
     return retVal;
   }
 }
+
+export const myMailService = new MyMailService();
